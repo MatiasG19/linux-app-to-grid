268e65d1c9488b8e95adc299e90aac16fd02bd07
MatiasG19 <m.galusek@gmail.com>
2023-05-16 22:17:17 +0200

    Initial commit

---
+++ AppToGrid.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+read -p "Enter application name (name will be shown in app grid): " appName
+
+read -p "Enter path to executable (full path with extension): " pathToExe
+
+fileNameWithExtension=$(echo "$pathToExe" | sed "s/.*\///")
+read -p "Copy executable $fileNameWithExtension to /usr/bin/ [y/n]?  " res
+
+if [ $res == "y" ]
+then
+echo "Copying executable $fileNameWithExtension to /usr/bin/..."
+cp $pathToExe "/usr/bin/"
+pathToExe="/usr/bin/$fileNameWithExtension"
+fi
+
+echo "[Desktop Entry]
+Type=Application
+Terminal=false
+Exec='$pathToExe'
+Name=$appName
+Comment=$appName
+Keywords=$appName;" > "/usr/share/applications/$appName.desktop"
+
+echo "Application added to grid."
+echo "/usr/share/applications/$appName.desktop"
+echo "Adding permissions..."
+chmod +x "/usr/share/applications/$appName.desktop"
+echo "Permissions added."
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# linux-app-to-grid
- Add application to grid
+# App to grid
+
+Simple command line tool to add applications to the app grid or list of your desktop environment.
+
+![Appgrid](./Appgrid.png)
+
+## How it works
+
+App to grid creates a `.desktop` file in `/usr/share/applications` for your application, so you can access it from the app grid or list of your desktop environment.
+
+Optionally it copies your binary to `/usr/bin`. That is useful for binaries like AppImages.
+
+## Getting started
+
+Enter `sudo ./AppToGrid.sh` into the terminal and follow the prompts.
+
+> Always enter the full application name and extension when the source application path is prompted, e.g. `/path/to/myApp.AppImage`
