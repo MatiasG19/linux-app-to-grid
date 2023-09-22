52e2e6259ad837d66ae2469d4a6d0473e1047018
MatiasG19 <m.galusek@gmail.com>
2023-09-22 21:45:05 +0200

    Add validation

--- AppToGrid.sh
+++ AppToGrid.sh
@@ -1,18 +1,41 @@
 #!/bin/bash
 
-read -rp "Enter application name (name will be shown in app grid): " appName
+while read -rp "Enter application name (name will be shown in app grid): " appName
+do
+    if [ "$appName" == "" ] 
+    then
+        echo "Application name cannot be empty."
+    else 
+        break
+    fi
+done
 
-read -rp "Enter path to executable (full path with extension): " pathToExe
+while read -rp "Enter path to executable (full path with extension): " pathToExe
+do
+    if [ ! -f "$pathToExe" ] 
+    then
+        echo "Executable not found."
+    else 
+        break
+    fi
+done
 
 fileNameWithExtension=$(echo "$pathToExe" | sed "s/.*\///")
-read -rp "Copy executable $fileNameWithExtension to /usr/bin/ [y/n]?  " res
-
-if [ "$res" == "y" ]
-then
-    echo "Copying executable $fileNameWithExtension to /usr/bin/..."
-    cp -i "$pathToExe" "/usr/bin/"
-    pathToExe="/usr/bin/$fileNameWithExtension"
-fi
+while read -rp "Copy executable $fileNameWithExtension to /usr/bin/ [y/n]?  " res
+do
+    if [ "$res" == "y" ]
+    then
+        echo "Copying executable $fileNameWithExtension to /usr/bin/..."
+        cp -i "$pathToExe" "/usr/bin/"
+        pathToExe="/usr/bin/$fileNameWithExtension"
+        break
+    elif [ "$res" == "n"  ] 
+    then
+        break
+    else 
+        echo "Invalid answer."
+    fi
+done
 
 echo "Adding permissions..."
 chmod +x "$pathToExe"
