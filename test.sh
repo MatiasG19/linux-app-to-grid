5d8ec4f0364bc61de32ce796e9705a571e682031
MatiasG19 <73800355+MatiasG19@users.noreply.github.com>
2023-09-22 02:36:56 +0200

    fix: Adding permissions (#1)
    * Add test
    
    * Update ci.yml
    
    * Update test.sh
    
    * Update test.sh
    
    * Update test.sh
    
    * Update test.sh
    
    * Update test.sh
    
    * Update test.sh
    
    * Update test.sh
    
    * Fix adding permissions
    
    * Update README.md
    
    * Add shellcheck linter
    
    * Update ci.yml
    
    * Add linting error
    
    * Revert "Add linting error"
    
    This reverts commit 01931e86dd4bd6ca18ee4a3a6e0cb137727b92fb.
    
    * Use specific ubuntu
    
    * Create dependabot.yml

---
+++ test.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+##################################
+# arrange
+##################################
+echo "#!/bin/bash
+echo Hello world!
+" > TestApp.sh
+
+##################################
+# act
+##################################
+printf '%s\n' TestApp ./TestApp.sh y | sudo ./AppToGrid.sh
+
+##################################
+# assert
+##################################
+## Check if executable exists
+if [ ! -f /usr/bin/TestApp.sh ]; then
+    echo "Assert 1" >> test.log
+    echo "Executable does not exist in /usr/bin/" >> test.log
+    echo
+fi
+
+## Check if desktop file exists
+if [ ! -f /usr/share/applications/TestApp.desktop ]; then
+    echo "Assert 2" >> test.log
+    echo ".desktop file does not exist in /usr/share/applications/" >> test.log
+    echo
+fi
+
+## .desktop file content
+echo "[Desktop Entry]
+Type=Application
+Terminal=false
+Exec='/usr/bin/TestApp.sh'
+Name=TestApp
+Comment=TestApp
+Keywords=TestApp;" > test.desktop
+diff <(cat test.desktop) <(cat /usr/share/applications/TestApp.desktop)
+if [ "$?" -eq 1 ] 
+then
+    {
+        echo "Assert 3" 
+        ".desktop file content is different"
+        diff <(cat test.desktop) <(cat /usr/share/applications/TestApp.desktop)
+    } >> test.log
+    echo
+fi
+
+## Check executable permissions
+if [ ! "$(ls -l /usr/bin/TestApp.sh | cut -c 10-10)" == "x" ]
+then
+    {
+        echo "Assert 4" 
+        echo "Executable has no execution permissions"
+        ls -l /usr/bin/TestApp.sh
+    } >> test.log
+    echo
+fi
+
+## Fail test when logfile exists
+if [ -f test.log ]; then
+    cat test.log
+    exit 1
+fi
