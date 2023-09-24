19c33e35d6e8b7dd95ebebda675e74610722d129
MatiasG19 <m.galusek@gmail.com>
2023-09-24 10:33:50 +0200

    Add remove action

--- AppToGrid.sh
+++ AppToGrid.sh
@@ -1,54 +1,132 @@
 #!/bin/bash
 
-while read -rp "Enter application name (name will be shown in app grid): " appName
+DESKTOP_DIR="/usr/share/applications/"
+BIN_DIR="/usr/bin/"
+
+PS3="Select action [1-2]: "
+select action in Add Remove
 do
-    if [ "$appName" == "" ] 
-    then
-        echo "Application name cannot be empty."
-    else 
-        break
-    fi
+    case $action in
+        "Add")
+            break;;
+        "Remove")
+            break;;
+        *)
+            echo "Invalid input.";;
+    esac
 done
 
-while read -rp "Enter path to executable (full path with extension): " pathToExe
-do
-    if [ ! -f "$pathToExe" ] 
+if [ "$action" == "Add" ]
+then
+    while read -rp "Enter application name (name will be shown in app grid): " appName
+    do
+        if [ "$appName" == "" ] 
+        then
+            echo "Application name cannot be empty."
+        else 
+            break
+        fi
+    done
+
+    while read -rp "Enter path to executable (full path with extension): " pathToExe
+    do
+        if [ ! -f "$pathToExe" ] 
+        then
+            echo "Executable not found."
+        else 
+            break
+        fi
+    done
+
+    fileNameWithExtension=$(basename "$pathToExe")
+    while read -rp "Copy executable $fileNameWithExtension to $BIN_DIR [y/n]?  " res
+    do
+        if [ "$res" == "y" ]
+        then
+            echo "Copying executable $fileNameWithExtension to $BIN_DIR..."
+            cp -i "$pathToExe" "$BIN_DIR"
+            pathToExe="$BIN_DIR$fileNameWithExtension"
+            break
+        elif [ "$res" == "n"  ] 
+        then
+            break
+        else 
+            echo "Invalid answer."
+        fi
+    done
+
+    echo "Adding permissions..."
+    chmod +x "$pathToExe"
+
+    echo "Adding application entry $appName.desktop to grid at $DESKTOP_DIR..."
+    echo "[Desktop Entry]
+    Type=Application
+    Terminal=false
+    Exec='$pathToExe'
+    Name=$appName
+    Comment=$appName
+    Keywords=$appName;" > "$appName.desktop"
+
+    mv -i "$appName.desktop" "$DESKTOP_DIR$appName.desktop"
+
+elif [ "$action" == "Remove" ]
+then
+    while read -rp "Enter application name (name in grid): " appName
+    do
+        if [ "$appName" == "" ] 
+        then
+            echo "Application name cannot be empty."
+        else 
+            break
+        fi
+    done
+
+    echo "Searching directories..."
+    shopt -s nullglob
+    desktopEntries=("$DESKTOP_DIR"*)
+    for desktopEntry in "${desktopEntries[@]}"
+    do
+        if grep -q "Name=$appName" "desktopEntry"
+        then
+            exeName="$(basename "$desktopEntry" ".desktop")"
+            break
+        fi
+    done
+
+    filesFound=()
+    if [ -f "$BIN_DIR$exeName" ] 
     then
-        echo "Executable not found."
-    else 
-        break
+        echo "Executable $exeName found in $BIN_DIR."
+        filesFound+=("$BIN_DIR$exeName")
     fi
-done
-
-fileNameWithExtension=$(echo "$pathToExe" | sed "s/.*\///")
-while read -rp "Copy executable $fileNameWithExtension to /usr/bin/ [y/n]?  " res
-do
-    if [ "$res" == "y" ]
+    if [ -f "$DESKTOP_DIR$exeName.desktop" ] 
     then
-        echo "Copying executable $fileNameWithExtension to /usr/bin/..."
-        cp -i "$pathToExe" "/usr/bin/"
-        pathToExe="/usr/bin/$fileNameWithExtension"
-        break
-    elif [ "$res" == "n"  ] 
-    then
-        break
-    else 
-        echo "Invalid answer."
+        echo "Desktop entry $exeName.dektop found in $DESKTOP_DIR."
+        filesFound+=("$DESKTOP_DIR$exeName.desktop")
     fi
-done
 
-echo "Adding permissions..."
-chmod +x "$pathToExe"
-
-echo "Adding application entry $appName.desktop to grid at /usr/share/applications/..."
-echo "[Desktop Entry]
-Type=Application
-Terminal=false
-Exec='$pathToExe'
-Name=$appName
-Comment=$appName
-Keywords=$appName;" > "$appName.desktop"
-
-mv -i "$appName.desktop" "/usr/share/applications/$appName.desktop"
+    if [ "${#filesFound[@]}" -gt 0 ]
+    then
+        while read -rp "Remove ${#filesFound[@]} files [y/n]?  " res
+        do
+            if [ "$res" == "y" ]
+            then
+                for file in "${!filesFound[@]}"
+                do
+                    echo "Removing $file..."
+                    rm "$file"
+                done
+                break
+            elif [ "$res" == "n"  ] 
+            then
+                break
+            else 
+                echo "Invalid answer."
+            fi
+        done
+    else
+        echo "Application not found."
+    fi
+fi
 
 echo "Done."
